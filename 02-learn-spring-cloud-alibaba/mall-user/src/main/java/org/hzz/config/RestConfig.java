24ff78aa7c5bd6af7fc448b91027fcda4a995f2d
zhuangzhuang-huang <cau1403090523@gmail.com>
2021-08-21 22:12:25 +0800

    Update RestConfig.java

--- 02-learn-spring-cloud-alibaba/mall-user/src/main/java/org/hzz/config/RestConfig.java
+++ 02-learn-spring-cloud-alibaba/mall-user/src/main/java/org/hzz/config/RestConfig.java
@@ -1,7 +1,9 @@
 package org.hzz.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -9,10 +11,15 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RestConfig {
 
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
     @Bean
-    @LoadBalanced
+//    @LoadBalanced
     public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder){
-        RestTemplate re = restTemplateBuilder.build();
-        return  re;
+
+        return restTemplateBuilder
+                .additionalInterceptors(new LoadBalancerInterceptor(loadBalancerClient))
+                .build();
     }
 }
