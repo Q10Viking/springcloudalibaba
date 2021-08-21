0d3b82d6ffecf245709d05ff64f1dcd287b92cf2
zhuangzhuang-huang <cau1403090523@gmail.com>
2021-08-21 20:17:28 +0800

    基础环境搭建

---
+++ learn-spring-cloud-alibaba/mall-user/src/main/java/org/hzz/controller/UserController.java
@@ -0,0 +1,25 @@
+package org.hzz.controller;
+
+import org.hzz.utils.R;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/findOrderByUserId/{id}")
+    public R findOrderByUserId(@PathVariable("id") Integer id){
+        String orderUrl = "http://localhost:8020/order/findOrderByUserId/"+id;
+        R r = restTemplate.getForObject(orderUrl, R.class);
+        return r;
+    }
+
+}
