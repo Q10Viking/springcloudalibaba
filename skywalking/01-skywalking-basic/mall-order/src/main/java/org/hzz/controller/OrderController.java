d085189a31ea290b3f0bf5db838f70407c2aac7d
静默 <cau1403090523@gmail.com>
2023-03-08 21:35:12 +0800

    finished all

---
+++ skywalking/01-skywalking-basic/mall-order/src/main/java/org/hzz/controller/OrderController.java
@@ -0,0 +1,28 @@
+package org.hzz.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.hzz.utils.R;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/order")
+@Slf4j
+public class OrderController {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${server.port}")
+    private int port;
+
+    @GetMapping("/findOrderByUserId/{id}")
+    public R findOrderByUserId(@PathVariable("id") Integer id){
+        log.info("current mall-order service port: {}",port);
+        return R.ok("maller-order"+ " port: "+ port + " id: "+id);
+    }
+}
