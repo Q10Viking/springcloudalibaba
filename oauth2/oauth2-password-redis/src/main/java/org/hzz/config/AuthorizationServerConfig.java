f1643f2c8d1a211552613dfb38d7b7df88ecf864
静默 <cau1403090523@gmail.com>
2023-03-13 21:46:02 +0800

    finished all

--- oauth2/oauth2-password-redis/src/main/java/org/hzz/config/AuthorizationServerConfig.java
+++ oauth2/oauth2-password-redis/src/main/java/org/hzz/config/AuthorizationServerConfig.java
@@ -1,18 +1,48 @@
 package org.hzz.config;
 
+import org.hzz.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
     @Autowired
+    private UserService userService;
+    @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private TokenStore tokenStore;
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        // 密码模式必须配置
+        endpoints.authenticationManager(authenticationManager)
+                .tokenStore(tokenStore)
+                .reuseRefreshTokens(false) // refresh_token是否重复使用
+                .userDetailsService(userService) // 刷新令牌授权包含对用户信息的检查
+                .allowedTokenEndpointRequestMethods(HttpMethod.GET,HttpMethod.POST); // 支持的方法
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        // 允许表单控制
+        security.allowFormAuthenticationForClients();
+    }
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
@@ -30,6 +60,6 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 //配置申请的权限范围
                 .scopes("all")
                 //配置grant_type，表示授权类型
-                .authorizedGrantTypes("authorization_code");
+                .authorizedGrantTypes("password","refresh_token");
     }
 }
--- oauth2/oauth2-password-redis/src/main/java/org/hzz/config/WebSecurityConfig.java
+++ oauth2/oauth2-password-redis/src/main/java/org/hzz/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.hzz.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,27 +13,26 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    private UserService userService;
-
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userService);
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.formLogin().permitAll()
                 .and().authorizeRequests()
                 .antMatchers("/oauth/**").permitAll()
-                .antMatchers("/order/**").permitAll()
                 .anyRequest().authenticated()
                 .and().logout().permitAll()
                 .and().csrf().disable();
+    }
+
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
